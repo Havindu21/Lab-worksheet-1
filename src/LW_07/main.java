3f65c3a7e40ec203af5d8acedbc40586a1ad3f74
Havindu Fonseka <havindufonseka@gmail.com>
2026-01-05 10:04:34 +0530

    LW_06 - LW_10

---
+++ src/LW_07/main.java
@@ -0,0 +1,8 @@
+package LW_07;
+
+public class main {
+    public static void main(String[] args) {
+        RegistrationForm registrationForm = new RegistrationForm();
+        registrationForm.setVisible(true);
+    }
+}
---
+++ src/LW_08/main.java
@@ -0,0 +1,8 @@
+package LW_08;
+
+public class main {
+    public static void main(String[] args) {
+        SignUpPage signUp = new SignUpPage();
+        signUp.setVisible(true);
+    }
+}
